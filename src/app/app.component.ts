8fe6eba0bb0f2e727ee7c972df89f5a1a7fd6bc6
Harvey Daclan <harvey@frontbyte.no>
2019-02-14 21:21:08 +0100

    -authentication and signup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './credentials';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+// import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from '@capacitor/core';
+
+const {SplashScreen, StatusBar} = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -11,16 +16,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    // private splashScreen: SplashScreen,
+    // private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.statusBar.styleDefault();
+      // this.splashScreen.hide();
+      SplashScreen.hide().catch(error => {
+        console.error(error);
+      });
+
+      StatusBar.hide().catch(error => {
+        console.error(error);
+      });
+
+      firebase.initializeApp(firebaseConfig);
+
     });
   }
 }
---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyB1g6V0-lox0U48JWZ8gbbwxxWB9zRexNY',
+    authDomain: 'hne-sandbox.firebaseapp.com',
+    databaseURL: 'https://hne-sandbox.firebaseio.com',
+    projectId: 'hne-sandbox',
+    storageBucket: 'hne-sandbox.appspot.com',
+    messagingSenderId: '2683618429'
+};
+
