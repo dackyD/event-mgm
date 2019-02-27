96d257944a61a158b03f7348ae39ec658969e63a
Harvey Daclan <harvey@frontbyte.no>
2019-02-27 21:25:30 +0100

    -profile page and service complete.

--- src/app/services/user/profile.service.ts
+++ src/app/services/user/profile.service.ts
@@ -1,9 +1,88 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  public userProfile: firebase.firestore.DocumentReference;
+  public currentUser: firebase.User;
+  constructor() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.currentUser = user;
+        this.userProfile = firebase.firestore().doc(`/userProfile/${user.uid}`);
+      }
+    });
+   }
+
+   /**
+    *
+    */
+   getUserProfile(): firebase.firestore.DocumentReference {
+     return this.userProfile;
+   }
+
+   /**
+    *
+    * @param firstName
+    * @param lastName
+    */
+   updateName(firstName: string, lastName: string): Promise<void> {
+     return this.userProfile.update({firstName, lastName});
+   }
+
+   /**
+    *
+    * @param birthDate
+    */
+   updateDOB(birthDate: string): Promise<any> {
+     return this.userProfile.update({birthDate});
+   }
+
+   /**
+    *
+    * @param newEmail
+    * @param password
+    */
+   updateEmail(newEmail: string, password: string): Promise<any> {
+      const credential: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(
+        this.currentUser.email,
+        password
+      );
+
+      return this.currentUser
+      .reauthenticateAndRetrieveDataWithCredential(credential)
+      .then(() => {
+        this.currentUser.updateEmail(newEmail).then(() => {
+          this.userProfile.update({email: newEmail});
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+   }
+
+   updatePassword(newPassword: string, oldPassword: string): Promise<any> {
+     const credential: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(
+       this.currentUser.email,
+       oldPassword
+     );
+
+     return this.currentUser
+     .reauthenticateAndRetrieveDataWithCredential(credential)
+     .then(() => {
+       this.currentUser.updatePassword(newPassword).then(() => {
+         console.log('Password Change');
+       });
+     })
+     .catch(error => {
+       console.error(error);
+     });
+   }
+
+
 }
